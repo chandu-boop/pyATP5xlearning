c65205270b70074527cde04f4a090ce487f909e2
chandu-boop <chandu955n@gmail.com>
2024-11-20 12:41:02 +0530

    NOV 16

---
+++ nov/14112024/16112024/lab013fizzbuzz.py
@@ -0,0 +1,10 @@
+for number in range(1,100):
+    if number%3==0:
+        print("fizz")
+    elif number%5==0:
+        print("buzz")
+    elif (number%3==0) and (number%5==0):
+        print("fizzbuzz")
+    else:
+        print("number")
+
